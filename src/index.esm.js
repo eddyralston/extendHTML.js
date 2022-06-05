fb27137302e14d8c49d69ec504d4cc0f4ef6da41
eddyralston <eddyralston@outlook.com>
2022-06-05 13:23:35 -0600

    first commit

---
+++ src/index.esm.js
@@ -0,0 +1,47 @@
+function getAttribute(host,attr,callback){
+    var els = host.querySelectorAll('['+attr+']');
+    var object = {};
+    var length = els.length;
+    for (var i = 0; i < length; i++) {
+        var name = els[i].getAttribute(attr);
+        callback(object,els[i],name);
+    }
+    return object;
+}
+function taggedString(str, ...args){
+    var result = str[0]; var len = args.length;
+    for (var i=0; i<len; i++) result += args[i] + str[i+1];
+    return result
+}
+function _html(str){
+    var el = document.createElement('div');
+    el.innerHTML = str
+    return el.firstElementChild;
+}
+
+function html (str, ...args){
+    var el = _html(taggedString(str, ...args));
+    el.extend = function(... args){
+        el.child = getAttribute(el, 'child', (obj,child,name)=>{
+            obj[name] = child;
+        });
+        el.data = getAttribute(el, 'data', (obj,child,name)=>{
+            child.addEventListener('input',()=>obj[name]=child.value);
+        });
+        el.methods = getAttribute(el,'method',(obj,child,name)=>{
+            child.addEventListener('click',()=>obj[name](this))
+        });
+        args.forEach(arg=>{
+            if(typeof arg === 'function'){
+                var methods = arg(el);
+                for (method in methods) el.methods[method] = methods[method];
+            }
+            else if (typeof arg === 'object'){
+                for (method in arg) el.methods[method] = arg[method];
+            }
+        })
+        return el
+    }
+    return el
+}
+export {html}
